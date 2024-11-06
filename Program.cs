b62310da079e32d255bc891b17892c36570f1f55
MayconL27 <mayconleandro2008@gmail.com>
2024-11-06 14:57:32 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Ler_Gravar_DadosCSV;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true
+        };
+
+        using (var reader = new StreamReader("../../../Data.csv"))
+        using (var csv = new CsvReader(reader, config))
+        {
+            csv.Context.RegisterClassMap<VendaMap>();
+
+            var vendas = csv.GetRecord<Venda>();
+
+            foreach (var venda in vendas)
+            {
+                Console.WriteLine($"Cliente: {venda.NomeCliente}. Pacote: {venda.NomePacote}");
+            }
+        }
+
+        Console.Read();
+    }
+}
